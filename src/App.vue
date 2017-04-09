e0a2fe7da56deff78332d5a8a86db3dbcaa1cedc
Ziyao Zhang <ziyaointl@gmail.com>
2017-04-10 01:02:01 +0800

    Import Bulma

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import "css/style.scss";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
