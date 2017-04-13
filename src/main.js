d22cf05e73386ed56ce34898195d042b3ec5bf72
Ziyao Zhang <ziyaointl@gmail.com>
2017-04-13 13:35:29 +0800

    Implement ProgressBar component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 
 Vue.component('sidebar', require('./Sidebar.vue'))
 Vue.component('topbar', require('./Topbar.vue'))
+Vue.component('progress-bar', require('./ProgressBar.vue'))
 
 new Vue({
   el: '#app',
