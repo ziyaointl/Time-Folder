4a9522dbb61c0a8529df5da16ef5c9cb57cd3e98
Ziyao Zhang <ziyaointl@gmail.com>
2017-04-18 09:29:05 +0800

    Switch to ionicons

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="sidebar">
-    <div class="sidebar-item">
+    <div class="sidebar-item is-active">
       <a href="#">
-        <h1><i class="fa fa-home fa-lg fa-fw"></i>All</h1>
+        <h1><i class="ion-home ion-fw"></i>All</h1>
       </a>
     </div>
     <div class="sidebar-item">
       <a href="#">
-        <h1><i class="fa fa-download fa-lg fa-fw"></i>Active</h1>
+        <h1><i class="ion-code-download ion-fw"></i>Active</h1>
       </a>
     </div>
     <div class="sidebar-item">
       <a href="#">
-        <h1><i class="fa fa-pause fa-lg fa-fw"></i>Paused</h1>
+        <h1><i class="ion-pause ion-fw"></i>Paused</h1>
       </a>
     </div>
     <div class="sidebar-item">
       <a href="#">
-        <h1><i class="fa fa-check fa-lg fa-fw"></i>Completed</h1>
+        <h1><i class="ion-checkmark-round ion-fw"></i>Completed</h1>
       </a>
     </div>
   </div>
--- src/Topbar.vue
+++ src/Topbar.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="topbar">
         <ul>
-            <li><h1><i class="fa fa-plus fa-lg fa-fw"></i>Add</h1></li>
-            <li><h1><i class="fa fa-pause fa-lg fa-fw"></i>Pause</h1></li>
-            <li><h1><i class="fa fa-download fa-lg fa-fw"></i>Active</h1></li>
+            <li><h1><i class="ion-plus"></i>Add</h1></li>
+            <li><h1><i class="ion-ios-pause"></i>Pause</h1></li>
+            <li><h1><i class="ion-trash-b"></i>Delete</h1></li>
         </ul>
     </div>
 </template>
