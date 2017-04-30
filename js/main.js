b8adc0492f77a4141ed968f27f3305412bdbdf8e
Ziyao Zhang <ziyaointl@gmail.com>
2017-04-30 20:03:58 +0800

    Implement Add button

--- js/main.js
+++ js/main.js
@@ -13,25 +13,46 @@ let aria2 = new Aria2(data);
 let app = new Vue({
   el: "#app",
   data: {
-    message: "Hello",
+    message: "",
     acitveDownloads: [],
-    pausedDownloads: []
+    pausedDownloads: [],
+    url: ""
   },
   mounted() {
     let vm = this;
 
     aria2.getVersion().then(
       function (res) {
-        vm.message = res;
+        vm.printString(JSON.stringify(res));
       },
       function (err) {
-        vm.message = err;
+        vm.printString(JSON.stringify(err));
       }
     );
   },
   methods: {
     openAddDialog() {
-      
+      let el = document.getElementById("addDialog");
+      el.classList.add("is-active");
+    },
+    closeAddDialog() {
+      let el = document.getElementById("addDialog");
+      el.classList.remove("is-active");
+    },
+    addLink(url) {
+      let vm = this;
+      aria2.addUri([url]).then(
+        function(res) {
+          vm.printString(JSON.stringify(res));
+        },
+        function(err) {
+          vm.printString(JSON.stringify(err));
+        }
+      )
+      vm.url = "";
+    },
+    printString(string) {
+      this.message += "<br>" + string;
     }
   }
 });
