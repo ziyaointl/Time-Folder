556a87804ff49fc2eeefc4068618470b41852804
Ziyao Zhang <ziyaointl@gmail.com>
2017-09-15 15:42:24 +0800

    Create tabComponents.js

--- js/test.js
+++ js/test.js
@@ -1,5 +1,6 @@
 exports.hello = function() {
   const taskComponent = require('./taskComponent.js')
+  const tabComponents = require('./tabComponents.js')
   const getName = require('./utility.js').getName
   const Aria2 = window.Aria2 === undefined ? require('aria2') : window.Aria2
 
@@ -32,84 +33,7 @@ exports.hello = function() {
   })
 
   taskComponent.initialize()
-
-  Vue.component('horizontal-tabs', {
-    template: '#horizontal-tabs-template',
-    methods: {
-      selectTab(tab) {
-        this.$children.forEach(currentTab => {
-          if (currentTab.name !== tab.name) {
-            currentTab.isActive = false
-          } else {
-            currentTab.isActive = true
-          }
-        })
-      }
-    }
-  })
-
-  Vue.component('horizontal-tab', {
-    template: '#horizontal-tab-template',
-    props: {
-      name: {
-        required: true
-      },
-      selected: {
-        default: false
-      }
-    },
-    data() {
-      return {
-        isActive: false
-      }
-    },
-    mounted() {
-      this.isActive = this.selected
-    }
-  })
-
-  Vue.component('tabs', {
-    template: '#tabs-template',
-    data() {
-      return {
-        tabs: []
-      }
-    },
-    created() {
-      this.tabs = this.$children
-    },
-    methods: {
-      selectTab(tab) {
-        this.tabs.forEach(tempTab => {
-          tempTab.isActive = (tempTab.name === tab.name)
-        })
-        store.commit('setCurrentTab', tab.name)
-      }
-    }
-  })
-
-  Vue.component('tab', {
-    template: '#tab-template',
-    props: {
-      name: {
-        required: true
-      },
-      selected: {
-        default: false
-      },
-      icon: {
-        required: true
-      }
-    },
-    data() {
-      return {
-        isActive: false
-      }
-    },
-    mounted() {
-      this.isActive = this.selected
-    }
-  })
+  tabComponents.initialize(store)
 
   let app = new Vue({
     el: "#app",
